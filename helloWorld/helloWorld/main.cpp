7c0218934c8f2ad0f73723261f5309e615e47c86
Alex Sottos <alexandersottos@gmail.com>
2020-04-22 22:58:15 -0500

    first commit test on hello world

---
+++ helloWorld/helloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  helloWorld
+//
+//  Created by Alexander Sottos on 4/22/20.
+//  Copyright © 2020 Alexander Sottos. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    cout << "Hello World" << endl;
+    return 0;
+}
